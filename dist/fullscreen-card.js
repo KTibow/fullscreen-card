b60a803fa73fda9845b521e960371742c97542f7
Kendell R <KTibow@users.noreply.github.com>
2020-08-25 12:15:49 -0700

    Update fullscreen-card.js

--- dist/fullscreen-card.js
+++ dist/fullscreen-card.js
@@ -2,18 +2,18 @@ class FullscreenCard extends HTMLElement {
   set hass(hass) {
     if (!this.content) {
       this.content = document.createElement("ha-card");
-      this.content.header = "Go fullscreen";
-      this.content.style.padding = "1.5em";
+      this.content.style.padding = "15px";
       this.atag = document.createElement("a");
-      this.atag.innerHTML = "Go";
-      this.atag.style.border = "2px solid grey";
+      this.atag.innerHTML = "Go fullscreen";
+      this.atag.style.border = "2px solid var(--primary-color)";
       this.atag.style.fontSize = "2em";
       this.atag.style.padding = "0.5em";
       this.atag.style.display = "block";
-      this.atag.style.margin = "15px 15px 30px 15px";
-      this.atag.style.background = "#8888";
+      this.atag.style.background = "var(--primary-color)";
+      this.atag.style.color = "white";
       this.atag.style.textAlign = "center";
-      this.atag.style.borderRadius = "5px";
+      this.atag.style.borderRadius = "var(--ha-card-border-radius, 4px)";
+      this.atag.style.cursor = "pointer";
       this.atag.onclick = function() {
         document.body.requestFullscreen();
       };
