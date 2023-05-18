f2f5186e2097302234a8547534381acb497e988b
Kendell R <KTibow@users.noreply.github.com>
2023-05-18 06:40:34 -0700

    update some stuff

--- README.md
+++ README.md
@@ -1,14 +1,16 @@
-# fullscreen-card [![hacs_badge](https://img.shields.io/badge/custom%20repo%20on-hacs-%2303a9f4.svg?style=flat-square)](https://hacs.xyz/)
+# fullscreen-card
+
 Make your Home Assistant browser fullscreen.  
 ![Screenshot](/fscreen.png)  
+[![hacs_badge](https://img.shields.io/badge/custom%20repo%20on-hacs-%2303a9f4.svg?style=flat-square)](https://hacs.xyz/)  
 ## install it
-Make sure [hacs](https://hacs.xyz/) is installed.  
-Then add this repo as a custom repository.  
-Then refresh your browser tab.  
-Then add the custom card to your dashboard.  
-Then click on ![go](https://img.shields.io/badge/-Go-%23888888.svg?style=flat-square).  
-Then you're ![done](https://img.shields.io/badge/-done-green.svg?style=flat-square).  
-Then consider installing the [![kiosk-mode addon](https://img.shields.io/badge/-kiosk--mode%20addon-orange.svg?style=flat-square)](https://github.com/NemesisRE/kiosk-mode).
+
+- make sure [hacs](https://hacs.xyz/) is installed
+- add this repo as a custom repository
+- refresh your tab
+- add the card to your dashboard
+- done! (consider installing [kiosk-mode](https://github.com/NemesisRE/kiosk-mode) too)
+
 ## so you want something different?
 ```
 type: "custom:fullscreen-card"
--- fullscreen-card.js
+++ fullscreen-card.js
@@ -1,37 +1,32 @@
-var the_card;
 class FullscreenCard extends HTMLElement {
   set hass(hass) {
     if (!this.content && this.config) {
       this.content = document.createElement("ha-card");
-      this.content.style.padding = "15px";
-      this.fullscreen = false;
-      this.atag = document.createElement("a");
-      this.atag.innerHTML = "Go fullscreen";
-      this.atag.style.border = "2px solid var(--primary-color)";
-      this.atag.style.fontSize = "2em";
-      this.atag.style.padding = "0.5em";
-      this.atag.style.display = "block";
-      this.atag.style.background = "var(--primary-color)";
-      this.atag.style.color = "white";
-      this.atag.style.textAlign = "center";
-      this.atag.style.borderRadius = "var(--ha-card-border-radius, 4px)";
-      this.atag.style.cursor = "pointer";
-      this.atag.onclick = function () {
-        if (this.fullscreen || window["fullScreen"] || document.fullscreenElement) {
+      this.content.style.padding = "1rem";
+
+      this.tag = document.createElement("button");
+      this.tag.innerHTML = this.config["go_fullscreen"] || "Go fullscreen";
+      this.tag.style.width = "100%";
+      this.tag.style.fontSize = "var(--paper-font-display1_-_font-size)";
+      this.tag.style.padding = "0.5rem";
+      this.tag.style.background = "var(--primary-color)";
+      this.tag.style.color = "var(--text-primary-color)";
+      this.tag.style.border = "none";
+      this.tag.style.textAlign = "center";
+      this.tag.style.borderRadius = "var(--ha-card-border-radius, 4px)";
+      this.tag.style.cursor = "pointer";
+      this.tag.onclick = function () {
+        if (window["fullScreen"] || document.fullscreenElement) {
           document.exitFullscreen();
-          this.fullscreen = false;
-          this.atag.innerHTML =
-            this.config["go_fullscreen"] || "Go fullscreen";
         } else {
           document.documentElement.requestFullscreen();
-          this.fullscreen = true;
-          this.atag.innerHTML =
-            this.config["exit_fullscreen"] || "Exit fullscreen";
         }
+        this.updateTag();
       }.bind(this);
-      this.content.appendChild(this.atag);
+
+      this.content.appendChild(this.tag);
       this.appendChild(this.content);
-      the_card = this;
+      document.addEventListener("fullscreenchange", this.updateTag.bind(this));
     }
   }
   setConfig(config) {
@@ -39,6 +34,15 @@ class FullscreenCard extends HTMLElement {
   }
   getCardSize() {
     return 2;
+  }
+  updateTag() {
+    if (window["fullScreen"] || document.fullscreenElement) {
+      this.tag.innerHTML =
+        this.config["exit_fullscreen"] || "Exit fullscreen";
+    } else {
+      this.tag.innerHTML =
+        this.config["go_fullscreen"] || "Go fullscreen";
+    }
   }
 }
 
@@ -50,9 +54,3 @@ window.customCards.push({
   preview: true,
   description: "Card to go fullscreen.",
 });
-document.body.onkeydown = (event) => {
-  if (event.key == "F11") {
-    event.preventDefault();
-    the_card.atag.onclick();
-  }
-};
